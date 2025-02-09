1c147477bcea6c18b62c8e5850d64644eeb95fd0
whs <hswongac@gmail.com>
2025-02-08 20:32:18 -0500

    Add request

---
+++ request/src/lib.rs
@@ -0,0 +1,106 @@
+use serde::{
+    Deserialize,
+    de::{self, Deserializer, SeqAccess, Visitor},
+};
+use std::collections::HashMap;
+use std::{fmt, marker::PhantomData, str::FromStr};
+use void::Void;
+
+#[derive(Debug, Deserialize)]
+pub struct ChatCompletionsRequest {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub frequency_penalty: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub logit_bias: Option<HashMap<String, String>>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub messages: Vec<Message>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub max_tokens: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub model: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub n: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub presence_penalty: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub stop: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub stream: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub temperature: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub top_p: Option<f32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub user: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Message {
+    #[serde(rename = "content", deserialize_with = "string_or_array")]
+    pub contents: Contents,
+    pub role: Role,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Role {
+    Assistant,
+    System,
+    User,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum Contents {
+    Array(Vec<Content>),
+    String(String),
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+pub enum Content {
+    #[serde(rename = "text")]
+    Text { text: String },
+}
+
+impl FromStr for Contents {
+    type Err = Void;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Contents::String(s.to_string()))
+    }
+}
+
+pub fn string_or_array<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: Deserialize<'de> + FromStr<Err = Void>,
+    D: Deserializer<'de>,
+{
+    struct StringOrArray<T>(PhantomData<fn() -> T>);
+
+    impl<'de, T> Visitor<'de> for StringOrArray<T>
+    where
+        T: Deserialize<'de> + FromStr<Err = Void>,
+    {
+        type Value = T;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("string or array")
+        }
+
+        fn visit_str<E>(self, value: &str) -> Result<T, E>
+        where
+            E: de::Error,
+        {
+            Ok(FromStr::from_str(value).expect("FromStr implementation for void cannot fail"))
+        }
+
+        fn visit_seq<S>(self, seq: S) -> Result<T, S::Error>
+        where
+            S: SeqAccess<'de>,
+        {
+            Deserialize::deserialize(de::value::SeqAccessDeserializer::new(seq))
+        }
+    }
+    deserializer.deserialize_any(StringOrArray(PhantomData))
+}
