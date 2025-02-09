941bca9be4cee3a0de75798d2d3ce106a7514d17
whs <hswongac@gmail.com>
2025-02-09 02:52:48 -0500

    Add choice and usage builder

--- response/src/lib.rs
+++ response/src/lib.rs
@@ -7,12 +7,12 @@ pub struct ChatCompletionsResponse {
     pub id: Option<String>,
     pub model: Option<String>,
     pub object: Option<String>,
-    pub usage: Usage,
+    pub usage: Option<Usage>,
 }
 
 #[derive(Debug, Serialize)]
 pub struct Choice {
-    pub delta: Delta,
+    pub delta: Option<Delta>,
     pub finish_reason: Option<String>,
     pub index: u32,
     pub logprobs: Option<String>,
@@ -27,8 +27,8 @@ pub enum Delta {
 
 #[derive(Debug, Default, Serialize)]
 pub struct Usage {
-    pub prompt_tokens: u32,
     pub completion_tokens: u32,
+    pub prompt_tokens: u32,
     pub total_tokens: u32,
 }
 
@@ -45,7 +45,7 @@ pub struct ChatCompletionsResponseBuilder {
     id: Option<String>,
     model: Option<String>,
     object: Option<String>,
-    usage: Usage,
+    usage: Option<Usage>,
 }
 
 impl ChatCompletionsResponseBuilder {
@@ -75,7 +75,7 @@ impl ChatCompletionsResponseBuilder {
     }
 
     pub fn usage(mut self, usage: Usage) -> Self {
-        self.usage = usage;
+        self.usage = Some(usage);
         self
     }
 
@@ -87,6 +87,77 @@ impl ChatCompletionsResponseBuilder {
             model: self.model,
             object: self.object,
             usage: self.usage,
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct ChoiceBuilder {
+    pub delta: Option<Delta>,
+    pub finish_reason: Option<String>,
+    pub index: u32,
+    pub logprobs: Option<String>,
+}
+
+impl ChoiceBuilder {
+    pub fn delta(mut self, delta: Delta) -> Self {
+        self.delta = Some(delta);
+        self
+    }
+
+    pub fn finish_reason(mut self, reason: String) -> Self {
+        self.finish_reason = Some(reason);
+        self
+    }
+
+    pub fn index(mut self, index: u32) -> Self {
+        self.index = index;
+        self
+    }
+
+    pub fn logprobs(mut self, logprobs: String) -> Self {
+        self.logprobs = Some(logprobs);
+        self
+    }
+
+    pub fn build(self) -> Choice {
+        Choice {
+            delta: self.delta,
+            finish_reason: self.finish_reason,
+            index: self.index,
+            logprobs: self.logprobs,
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct UsageBuilder {
+    pub completion_tokens: u32,
+    pub prompt_tokens: u32,
+    pub total_tokens: u32,
+}
+
+impl UsageBuilder {
+    pub fn completion_tokens(mut self, tokens: u32) -> Self {
+        self.completion_tokens = tokens;
+        self
+    }
+
+    pub fn prompt_tokens(mut self, tokens: u32) -> Self {
+        self.prompt_tokens = tokens;
+        self
+    }
+
+    pub fn total_tokens(mut self, tokens: u32) -> Self {
+        self.total_tokens = tokens;
+        self
+    }
+
+    pub fn build(self) -> Usage {
+        Usage {
+            prompt_tokens: self.prompt_tokens,
+            completion_tokens: self.completion_tokens,
+            total_tokens: self.total_tokens,
         }
     }
 }
