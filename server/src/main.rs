318da058bfa7f7be8dbcbff3754a5e09a71f9c65
whs <hswongac@gmail.com>
2025-02-10 02:47:15 -0500

    Add config

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,6 +7,7 @@ use chat::{
     error::StreamError,
     providers::{BedrockChatCompletionsProvider, ChatCompletionsProvider},
 };
+use config::{Config, File};
 use futures::stream::Stream;
 use request::ChatCompletionsRequest;
 
@@ -29,9 +30,16 @@ async fn chat_completions(
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
+    let settings = Config::builder()
+        .add_source(File::with_name("config"))
+        .build()?;
+
+    let host: String = settings.get("host")?;
+    let port: u16 = settings.get("port")?;
+
     let app = Router::new().route("/chat/completions", post(chat_completions));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    let listener = tokio::net::TcpListener::bind(format!("{}:{}", host, port)).await?;
     axum::serve(listener, app).await?;
 
     Ok(())
