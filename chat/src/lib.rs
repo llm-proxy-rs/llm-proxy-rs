e615d00113973560113abe11c3576df7fa53c121
whs <hswongac@gmail.com>
2026-01-19 07:14:47 +0000

    Remove unused trait

--- chat/src/lib.rs
+++ chat/src/lib.rs
@@ -6,13 +6,6 @@ use response::ChatCompletionsResponse;
 
 pub const DONE_MESSAGE: &str = "[DONE]";
 
-pub trait ProcessChatCompletionsRequest<T> {
-    fn process_chat_completions_request(
-        &self,
-        request: &request::ChatCompletionsRequest,
-    ) -> anyhow::Result<T>;
-}
-
 fn create_sse_event(response: &ChatCompletionsResponse) -> anyhow::Result<Event> {
     match serde_json::to_string(response) {
         Ok(data) => Ok(Event::default().data(data)),
